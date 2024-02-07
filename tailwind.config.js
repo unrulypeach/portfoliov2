4415b96054e142b62034c04e27aa23241f68d8a1
lc <gingeraleisthebest@gmail.com>
2024-02-06 23:25:56 -0500

    moved common colors to variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
-// eslint-disable-next-line no-undef
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      ...colors,
+      title: '#b7c7e8',
+      borderGrey: '#90a1c3',
+    },
     extend: {},
   },
   plugins: [require('daisyui')],
