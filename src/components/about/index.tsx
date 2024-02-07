cfda78e62bd1d2d42d1eba0e6f577fc10ca551b5
lc <gingeraleisthebest@gmail.com>
2024-02-06 23:28:31 -0500

    implemented about pg w/ mock content

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,7 +1,17 @@
+import AboutText from './features/AboutText';
+import testimg from '../../../public/moon.webp';
+import DonutAnimation from './features/DonutAnimation';
+
 function About() {
   return (
     <div id="about" className="h-screen">
-      About
+      <div className="flex p-16 h-[inherit]">
+        {/* <DonutAnimation /> */}
+        <div className="flex-1 pr-4 h-[inherit]">
+          <img src={testimg} className="object-cover h-[inherit] pb-32" alt="" />{' '}
+        </div>
+        <AboutText />
+      </div>
     </div>
   );
 }
