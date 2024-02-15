4d7e5bb7294ca6875058d5196e9841f80d7a705e
lc <gingeraleisthebest@gmail.com>
2024-02-15 17:33:57 -0500

    created mobile home page

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,14 +1,16 @@
 import JumpToTop from 'components/features/JumpToTop';
 import Header from './features/Header';
 import Title from './features/Title';
+import TitleMobile from './features/TitleMobile';
 
 function Home() {
+  const screenW = window.innerWidth;
   return (
     <>
       <JumpToTop />
       <div id="home" className="bg-bg flex flex-col justify-between h-screen snap-center">
         <Header />
-        <Title />
+        {screenW > 800 ? <Title /> : <TitleMobile />}
       </div>
     </>
   );
