94c9a999a88092a08d15406a14b38307391ba0fe
lc <gingeraleisthebest@gmail.com>
2024-02-19 22:37:23 -0500

    fixed type

--- src/components/home/features/useDimensions.tsx
+++ src/components/home/features/useDimensions.tsx
@@ -15,8 +15,10 @@ export const useDimensions = (
   const dimensions = useRef({ width: 0, height: 0 });
 
   useEffect(() => {
-    dimensions.current.width = ref.current.offsetWidth;
-    dimensions.current.height = ref.current.offsetHeight;
+    if (ref.current) {
+      dimensions.current.width = ref.current.offsetWidth;
+      dimensions.current.height = ref.current.offsetHeight;
+    }
   }, []);
 
   return dimensions.current;
