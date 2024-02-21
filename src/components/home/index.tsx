f30ebb0e10a906b1af1f77d01f29ddebd1fd366d
lc <gingeraleisthebest@gmail.com>
2024-02-21 14:04:54 -0500

    fixed resize issue

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -21,7 +21,9 @@ function Home() {
   });
 
   useEffect(() => {
-    window.addEventListener('load', handleResize);
+    window.addEventListener('load', () => {
+      handleResize();
+    });
   }, []);
 
   return (
